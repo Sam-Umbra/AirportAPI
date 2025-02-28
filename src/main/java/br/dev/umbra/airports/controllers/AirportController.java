7e2d87caee6c4fc98e0d139fdfcfc443d2c6b0e0
Sam-Umbra <samuuel2008@gmail.com>
2025-02-28 08:33:19 -0300

    Término da Fase 2 -Implementado endpoint Airport -Add interface AirportRepository -Add classe AirportService -Add classe AirportController

---
+++ src/main/java/br/dev/umbra/airports/controllers/AirportController.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.dev.umbra.airports.controllers;
+
+import br.dev.umbra.airports.entities.Airport;
+import br.dev.umbra.airports.service.AirportService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author sesidevb
+ */
+
+@RestController
+public class AirportController {
+    
+    @Autowired
+    private AirportService airportService;
+    
+    /**
+     * Endpoint /airports/airport
+     * Retorna TODOS os aeroportos da base de dados.
+     * @return
+     */
+    @GetMapping("/airport")
+    public List<Airport> findAll() {
+        List<Airport> result = airportService.findAll();
+        return result;
+    }
+    
+}
