f8e7e246584749b8efca1b15ecbf8dd82098d5df
Sam-Umbra <samuuel2008@gmail.com>
2025-02-28 08:57:18 -0300

    Término da Fase 3 -Implementado endpont FindByCityIgnoreCase. -Update AirportRepository - Aicionado assinatura de método findByCityIgnoreCase; -Update AirportService - Adicionado método findByCity -Update AirportController - Adicionado endpoint /city/{cityname}

--- src/main/java/br/dev/umbra/airports/controllers/AirportController.java
+++ src/main/java/br/dev/umbra/airports/controllers/AirportController.java
@@ -8,7 +8,9 @@ import br.dev.umbra.airports.entities.Airport;
 import br.dev.umbra.airports.service.AirportService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -31,6 +33,29 @@ public class AirportController {
     public List<Airport> findAll() {
         List<Airport> result = airportService.findAll();
         return result;
+    }
+    
+    /**
+     * Endpoint /airports/city/{cityName}
+     * preparado para devolver código de status conforme
+     * padronização REST.
+     * @param cityName
+     * @return
+     */
+    @GetMapping("/city/{cityName}")
+    public ResponseEntity<List<Airport>> findByCityIgnoreCase(@PathVariable String cityName) {
+        List<Airport> result = airportService.findByCity(cityName);
+        
+        if (result.isEmpty()) {
+            // Código de Resposta
+            // notFound devolve 404
+            return ResponseEntity.notFound().build();
+            
+        } else {
+            // ok devolve 200
+            return ResponseEntity.ok(result);
+        }
+        
     }
     
 }
--- src/main/java/br/dev/umbra/airports/service/AirportService.java
+++ src/main/java/br/dev/umbra/airports/service/AirportService.java
@@ -27,4 +27,15 @@ public class AirportService {
         
     }
     
+    /**
+     * Retorno DTO Airports filtrado por cidade.
+     * @param city
+     * @return
+     */
+    public List<Airport> findByCity(String city) {
+        List<Airport> result = airportRepository.findByCityIgnoreCase(city);
+        return result;
+    }
+    
+    
 }
